12c307c45def89d8a7751223041d423c6af42c79
pqhwan <hyun_sik_kim@brown.edu>
2013-10-03 19:19:02 -0400

    pseudo updated, main function first draft completed, get socket completed

--- node.c
+++ node.c
@@ -28,14 +28,94 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <errno.h>
+#include <stdbool.h>
 
 
 #define RECVBUFSIZE 65536
+#define CMDBUFSIZE 1024
 #define LOCALDELIVERY 1
 #define FORWARD 0
 
+typedef struct{
+	int id;
+	int sockfd;
+	struct sockaddr sourceaddr;
+	struct sockaddr destaddr;
+	uint32_t sourcevip;
+	uint32_t destvip;
+	bool status;
+}interface_t;
+
+typedef struct {
+	uint32_t cost;
+	uint32_t addr;
+	uint32_t nexthop;
+	time_t refreshtime;
+	bool local;
+}rtu_routing_entry;
 
 
+
+	void
+print_interfaces () 
+{
+
+
+
+}		/* -----  end of function print_interface  ----- */
+
+
+	void
+print_routes () 
+{
+
+
+}		/* -----  end of function print_routes  ----- */
+
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  get_socket
+ *  Description:  
+ * =====================================================================================
+ */
+	int
+get_socket (char *port, struct addrinfo *source, int type, int local)
+{
+	struct addrinfo *p;
+	int sockfd, yes = 1;
+
+	if(get_addr(port, source, type, local) == -1){
+		printf("get_addr()\n");
+		exit(1);
+	}
+ 
+	for(p = source; p!=NULL; p=p->ai_next){
+		if((sockfd= socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
+			perror("socket()");
+			continue;
+		}
+		if(bind(sockfd, p->ai_addr, p->ai_addrlen) != 0){
+			perror("bind()");
+			close(sockfd);
+			continue;
+		}
+
+		if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1){
+			perror("setsockopt()");
+			exit(1);
+		}
+		break;
+	}
+
+	return sockfd;
+}		/* -----  end of function get_socket  ----- */
+
+
+
+struct interface_t *only_interface;
+struct rtu_routing_entry *only_entry;
 
 /* 
  * ===  FUNCTION  ======================================================================
@@ -46,6 +126,71 @@
 	int
 main ( int argc, char *argv[] )
 {
+
+	if(argc < 1){
+		printf("usage: node lnxfilename\n");
+		exit(1);
+	}
+
+
 	
+	//<-SETUP->
+	if(setup_interface(argv[1]) == -1){
+		printf("setup_interface() went wrong\n");
+		exit(1);
+	}
+
+	only_entry = local_routing_setup(only_interface);
+
+	//<-WARNING->Intentionally did not set up FD for the socket: not necessary at this point
+	fd_set readfds;
+	fd_set masterfds;
+	FD_ZERO(&masterfds);
+	FD_ZERO(&readfds);
+	FD_SET(0, &masterfds);
+	struct timeval tv, tvcopy;
+	tv.tv_sec = 1;
+	tv.tv_usec = 0;
+	maxfd = 2;
+
+	char command[CMDBUFSIZE];
+	int command_bytes;
+	while(1){
+		readfds = masterfds;
+		tvcop = tv;
+		if(select(maxfd+1, &readfds, NULL, NULL, &tvcop) == -1){
+			perror("select()");
+			exit(1);
+		}
+
+		printf("select() released\n");
+		if(FD_ISSET(0)){
+			command_bytes = read(0, command, CMDBUFSIZE);
+
+			if(command_bytes == -1){
+				perror("read");
+				exit(-1);
+			}
+
+			command[command_bytes] = '\0';
+
+			if(strcmp("routes", command) || strcmp("r",command)){
+				print_routes();
+			}
+			
+			if(strcmp("interfaces", command) || strcmp("i", command)){
+				print_interfaces();
+			}
+		}
+
+	}
+
+
+	
+
+
+
+
+
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
